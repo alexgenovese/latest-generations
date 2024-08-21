adde147915334d73ce4f4597c822d648c35d3780
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2024-08-21 16:32:25 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,12 +51,15 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           <div className="after:content relative mb-5 flex h-[650px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <Logo />
-            <h1 className="mt-8 mb-4 text-2xl text-black font-bold uppercase">
+            <h1 className="mt-8 mb-4 text-3xl text-black font-bold uppercase">
               Latest generations
             </h1>
+            <p className="max-w-[40ch] text-black text-2xl sm:max-w-[32ch]">
+              A wall of FREE photo and ai photo generations for your adv!
+            </p>
+            <hr/>
             <p className="max-w-[40ch] text-black/85 text-xl sm:max-w-[32ch]">
-              Reica stunning generations for your marketing adv assets, 
-              or anything you need!
+              Generate AI Photo for free! Sign up and get FREE credits to test upscaler or generate images for your next marketing campaign.
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-blue-600 px-6 py-4 text-lg font-semibold text-white transition hover:bg-blue-600/80 hover:text-white md:mt-4"
