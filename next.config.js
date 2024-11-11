4ffc0f6072d6025b747f4239ee175bd5a04085db
Alex <423745+alexgenovese@users.noreply.github.com>
2024-11-11 12:43:03 +0100

    added social icons

--- next.config.js
+++ next.config.js
@@ -25,18 +25,11 @@ const nextConfig = {
     cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     slug_link: "latest-generations",
     url: "latest-generations.getreica.com",
-    title_og: "Reica | Generate AI image for Free",
-    desc_og: "AI Generative photorealistic suite for Marketeers | Try and generate photo free, Get Free Credits to Generate your next asset",
+    title_og: "Reica | Quickly generate realistic photos for marketeer",
+    desc_og: "AI Generative suite for fast moving teams and brands",
     supabase_api_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbG1oa2xkaXNvcnNncG9uaXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ0MzMyMjEsImV4cCI6MjA0MDAwOTIyMX0.Q8eqlJCRggK_1fP6BAIFThvKVgJEYEoS3xdSOjXmqYA",
     supabase_url: "https://ddlmhkldisorsgponisj.supabase.co",
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    return config
-  },
+  }
 };
 
 module.exports = nextConfig
